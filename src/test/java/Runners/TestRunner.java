db73dc3531bc71c96af681cf42ef9056f163b42f
Priyabrata Roy <rpriyabrata11@gmail.com>
2026-05-28 17:47:42 +0530

    done

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/02_addToCart.feature",
+        features = "src/test/resources/features/01_search.feature",
         glue = "stepdefinitions",
         plugin = {
                 "pretty",
